861ab66851e70a617550f543148283b233feb78b
Julius <julius.bichage@gmail.com>
2015-04-24 10:16:26 +0100

    Initial checkin of src

---
+++ CodingQuiz/src/CodingQuiz.Model/Answer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingQuiz.Model
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }//FK to Question
+        [Required]
+        public string AnswerText { get; set; }
+        public int Weight { get; set; }
+        public bool IsAnswer { get; set; }
+
+    }
+}
---
+++ CodingQuiz/src/CodingQuiz.Model/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingQuiz.Model
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        [Required]
+        public string QuestionText { get; set; }
+        public string ImgUrl { get; set; }
+        /*
+        QuestionType
+        List<Answer>
+        */
+
+    }
+}
